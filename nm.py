d85167d5de2a652da6e850efecc70f23f730f2a3
betelihem <betelihem2127@gmail.com>
2022-12-29 16:11:30 +0300

    this is done by betelihem

---
+++ nm.py
@@ -0,0 +1,27 @@
+def sum():
+    # this is the first comment
+    x,y,z="orange","banana","cherry"
+    a=b=c="orange"
+    print("hi",a,b,c,x,y,z)
+    print(a)
+    print(b)
+    print(c)
+    print(x)
+    print(y)
+    print(z)
+sum()
+a=2
+b=2.8
+print(type(b))
+x="dfghjjiuuuuuyg"
+print(len(x))
+d=input(" please enter youer name")
+print("hello",d)
+x=input(" please enter the first num")
+y=input(" please enter second num")
+sum=int(x)+int(y)
+print(sum)
+name="Betelihem"
+age=23
+txt="my name is {:s} and I am {:d} years old" 
+print(txt.format(name,age))
