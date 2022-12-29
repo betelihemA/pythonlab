07025b2459a4769544e6d01d56909de919e03a8a
Girminiyo <kraken967177@gmail.com>
2022-12-29 16:10:40 +0300

    python
    this is first python lab pracice

---
+++ labpractice1.py
@@ -0,0 +1,31 @@
+###this is comment section
+'''dsdds'''
+### x,y,z="Orange","Banana","Cherry"
+### print(x)
+### print(y)
+### print(z)
+### x=y=z="Orange"
+### print(x)
+### print(y)
+### print(z)
+### a=2
+###b=2.1
+###print(type(a))
+###print(type(b))
+###c="haymanot"
+###print(len(c))
+###d=input("Enter your name: ")
+###print(d)
+###print(type(d))
+###e=input("Enter first number: ")
+###f=input("Enter second number: ")
+###g=int(e)+int(f)
+###print(g)
+###print(ord('g'))
+name="someone"
+age=23
+txt="My name is {:s} and I am {:d} years old"
+print(txt.format(name,age))
+def sum():
+    print("Hello")
+sum()
